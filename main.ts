4af360daa217131a977ed7ac1b47c8c2ffe4cddc
mcmodmax <38328111+mcmodmax@users.noreply.github.com>
2022-02-21 08:43:26 +0100

    Ampel Sync

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 98765) {
+        Ampel = 1
+    } else {
+        if (receivedNumber == 46123) {
+            Ampel += 2
+        }
+    }
+})
 /**
  * Ampel An
  */
@@ -12,7 +21,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (Ampel == 1) {
-        Ampel += 2
+        Ampel = 2
         radio.sendNumber(66453)
     }
 })
