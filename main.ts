4515aaf45b2caf14a83b93f68cc4d883d87446d5
mcmodmax <38328111+mcmodmax@users.noreply.github.com>
2022-02-28 08:49:26 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,13 +17,16 @@ input.onButtonPressed(Button.A, function () {
     if (Ampel == 2) {
         Ampel = 1
         radio.sendNumber(93847)
+        radio.sendNumber(98765)
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (Ampel == 1) {
         Ampel = 2
         radio.sendNumber(66453)
+        radio.sendNumber(46123)
     }
 })
 let Ampel = 0
 radio.setGroup(187)
+Ampel = 2
