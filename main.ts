c20c5c33909d857f17c2ccbde98c954fa9ebed1f
mcmodmax <38328111+mcmodmax@users.noreply.github.com>
2022-02-28 08:54:11 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,16 +15,16 @@ radio.onReceivedNumber(function (receivedNumber) {
  */
 input.onButtonPressed(Button.A, function () {
     if (Ampel == 2) {
+        radio.sendNumber(98765)
         Ampel = 1
         radio.sendNumber(93847)
-        radio.sendNumber(98765)
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (Ampel == 1) {
+        radio.sendNumber(46123)
         Ampel = 2
         radio.sendNumber(66453)
-        radio.sendNumber(46123)
     }
 })
 let Ampel = 0
